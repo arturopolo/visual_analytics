9a29ba6473d8eb27818fb36838e8f752f798c22b
Christian Poveda <christianpoveda@protonmail.com>
2017-10-20 14:03:25 -0500

    changed domain of barchart to show bars of height 1

--- projects/hw3_src/src/components/barchart.js
+++ projects/hw3_src/src/components/barchart.js
@@ -4,7 +4,7 @@ import React from 'react';
 const Barchart = ({ data, onBarClick }) => {
   return (
     <div>
-      <XYPlot width={1024} height={200} yDomain={[1, 2000]} yType="log">
+      <XYPlot width={1024} height={200} yDomain={[0.5, 2000]} yType="log">
         <XAxis />
         <YAxis tickFormat={Math.round} tickValues={[1, 10, 100, 1000]}/>
         <VerticalBarSeries data={data} onValueClick={onBarClick}/>
